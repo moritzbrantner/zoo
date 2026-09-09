e71b78787778219a031dcc708f67ebcc54dc3d87
moritzbrantner <31051084+moritzbrantner@users.noreply.github.com>
2026-09-09 08:24:19 +0200

    Mount the 3D camera layer

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,6 +1,7 @@
 import {StrictMode} from "react"
 import {createRoot} from "react-dom/client"
 import App from "./App"
+import Park3DCamera from "./Park3DCamera"
 import ParkFrameOverlay from "./ParkFrameOverlay"
 import {installTouchTileDragSupport} from "./touch-input"
 import "./styles.css"
@@ -9,6 +10,7 @@ import "./fence.css"
 import "./park-frame.css"
 import "./concessions.css"
 import "./mobile.css"
+import "./three-d.css"
 
 installTouchTileDragSupport()
 
@@ -16,5 +18,6 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
     <ParkFrameOverlay />
+    <Park3DCamera />
   </StrictMode>,
 )
